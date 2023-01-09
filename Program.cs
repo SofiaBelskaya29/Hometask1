04b06c73094e51979f7de3b1f82e532f5124d694
sofia_belskaya <sofi2993@yandex.ru>
2023-01-09 19:35:57 +0300

    Задача1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end);
+    return number;
+}
+int WriteSecondDigit (int number)
+{
+    int result = ((number / 10)% 10);
+    return result;
+}
+int randomNumber = GetNumberFromRange(100,999);
+int numberSecond = WriteSecondDigit(randomNumber);
+Console.WriteLine($"У числа {randomNumber} второе число {numberSecond}");
